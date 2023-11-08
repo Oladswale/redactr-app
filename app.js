c0d1fd6d73502f97dc0dfca37531fd6ebfcaf0dd
oladswale <oladswale@gmail.com>
2023-11-08 16:35:46 +0100

    My second commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/scramble", (req, res) => {
+  const text = req.body.text;
+  const wordsToScramble = req.body.words.split(" ");
+  const scrambledText = text
+    .split(" ")
+    .map((word) => {
+      if (wordsToScramble.includes(word)) {
+        return word
+          .split("")
+          .sort(() => Math.random() - 0.5)
+          .join("");
+      }
+      return word;
+    })
+    .join(" ");
+
+  res.send(scrambledText);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
