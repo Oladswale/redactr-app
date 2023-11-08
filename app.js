b1ffae774258cfa2031eab3440f4b798166ba1af
oladswale <oladswale@gmail.com>
2023-11-08 16:48:06 +0100

    My third commit

--- app.js
+++ app.js
@@ -8,24 +8,20 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/scramble", (req, res) => {
+app.post('/scramble', (req, res) => {
   const text = req.body.text;
-  const wordsToScramble = req.body.words.split(" ");
-  const scrambledText = text
-    .split(" ")
-    .map((word) => {
-      if (wordsToScramble.includes(word)) {
-        return word
-          .split("")
-          .sort(() => Math.random() - 0.5)
-          .join("");
-      }
-      return word;
-    })
-    .join(" ");
+  const wordsToScramble = req.body.words.split(' ');
+  const scrambledText = text.split(' ').map(word => {
+    if (wordsToScramble.includes(word)) {
+      // Replace the word with asterisks
+      return '*'.repeat(word.length);
+    }
+    return word;
+  }).join(' ');
 
   res.send(scrambledText);
 });
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
